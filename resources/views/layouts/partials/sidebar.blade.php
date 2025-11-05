1f3c6bd63817d63cf2b54e42b2c48dc3cd83c12d
Minh Tran <trcongminh.261002@gmail.com>
2025-11-05 17:34:03 +0700

    Update sidebar to display dynamic user information, including avatar, name, and role. Replace static links with a logout form for improved user experience.

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -64,20 +64,22 @@
             <div class="nav-item dropdown">
                 <a href="#" class="nav-link d-flex lh-1 p-0 px-2" data-bs-toggle="dropdown"
                     aria-label="Open user menu">
-                    <span class="avatar avatar-sm" style="background-image: url(./static/avatars/000m.jpg)">
+                    <span class="avatar avatar-sm" style="background-image: url({{ $user->avatar }})">
                     </span>
                     <div class="d-none d-xl-block ps-2">
-                        <div>Paweł Kuna</div>
-                        <div class="mt-1 small text-secondary">UI Designer</div>
+                        <div>
+                            {{ $user->name }}
+                        </div>
+                        <div class="mt-1 small text-secondary">
+                            {{ $user->role[0]->title }}
+                        </div>
                     </div>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    <a href="#" class="dropdown-item">Status</a>
-                    <a href="./profile.html" class="dropdown-item">Profile</a>
-                    <a href="#" class="dropdown-item">Feedback</a>
-                    <div class="dropdown-divider"></div>
-                    <a href="./settings.html" class="dropdown-item">Settings</a>
-                    <a href="./sign-in.html" class="dropdown-item">Logout</a>
+                    <form action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <button type="submit" class="dropdown-item">Đăng xuất</button>
+                    </form>
                 </div>
             </div>
         </div>
