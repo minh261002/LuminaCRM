2fc4900e6b7bed24b66ae7aecb216eb6972a4ac9
Minh Tran <trcongminh.261002@gmail.com>
2025-11-05 11:55:30 +0700

    Enhance Module model by adding factory support, defining table name, and implementing guarded attributes; introduce casting for status using ModuleStatus enum for improved data handling.

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use App\Enums\ModuleStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Module extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'modules';
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'status' => ModuleStatus::class,
+    ];
 }
